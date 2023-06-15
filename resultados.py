f2161f03f4e6d206127de84d6e5959b14a260430
Jerry <jerrynalam2@gmail.com>
2023-06-15 10:43:49 -0600

    Se inico el archivo

---
+++ resultados.py
@@ -0,0 +1,15 @@
+from seintento import *
+
+def main():
+    print("1. Suma")
+    print("2. Resta")
+    print("3. Multiplicación")
+    print("4. División")
+    print("5. Exponenciación")
+    print("6. Raíz")
+    print("7. Suma de 5 números")
+    print("8. Residuo")
+    print("9. Quetzal a Dólar")
+    print("10. Dólar a Quetzal")
+    
+    opcion = int(input("Ingrese una opción:"))
